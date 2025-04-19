0bef5a85bfe76e8ec5fc1528e42d68f0f4817e98
nidhi-builds <nidhipandit2907@gmail.com>
2025-04-19 20:40:35 +0530

    Add Main.java via upload

---
+++ Main.java
@@ -0,0 +1,40 @@
+/*
+Name: Your Name
+PRN: 1234567890
+Batch: SY BTech AIML B1
+*/
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int choice;
+
+        do {
+            System.out.println("\n--- Student Data Entry System ---");
+            System.out.println("1. Add Student");
+            System.out.println("2. View Student");
+            System.out.println("3. Update Student");
+            System.out.println("4. Delete Student");
+            System.out.println("5. Exit");
+            System.out.print("Enter choice: ");
+            choice = sc.nextInt();
+
+            try {
+                switch (choice) {
+                    case 1 -> AddStudent.add();
+                    case 2 -> ViewStudent.view();
+                    case 3 -> UpdateStudent.update();
+                    case 4 -> DeleteStudent.delete();
+                    case 5 -> System.out.println("Exiting...");
+                    default -> System.out.println("Invalid option!");
+                }
+            } catch (InvalidInputException | StudentNotFoundException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        } while (choice != 5);
+
+        sc.close();
+    }
+}
