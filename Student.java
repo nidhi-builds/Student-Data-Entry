70bc30aa8720370ca26a770a0760674add02472b
nidhi-builds <nidhipandit2907@gmail.com>
2025-04-19 20:38:49 +0530

    Add Student.java via upload

---
+++ Student.java
@@ -0,0 +1,12 @@
+// POJO class for student details
+public class Student {
+    public static int id;
+    public static String name;
+    public static String email;
+
+    public Student(int id, String name, String email) {
+        Student.id = id;
+        Student.name = name;
+        Student.email = email;
+    }
+}
